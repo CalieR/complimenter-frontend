335312104e69b238325d1d641727cc055bd4f764
github username <github email address>
2019-05-07 14:50:39 +0100

    create image in form, added user_id to post params

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import "../App.css";
-import Compliment from './Compliment'
-import UrlForm from './Form'
+import Compliment from "./Compliment";
+import UrlForm from "./Form";
 
 const COMP_URL = "http://localhost:3002/api/v1/compliments";
-
 
 class App extends Component {
   state = {
     currentCompliment: "",
-    imgUrl:""
+    currentImage: null
   };
 
   componentDidMount() {
@@ -22,14 +21,15 @@ class App extends Component {
       );
   }
 
-  render (){
-  return (
-    <div className="App">
-      <h1>Compliments here:</h1>
-      <Compliment currentCompliment={this.state.currentCompliment}/>
-      <UrlForm imgUrl={this.state.imgUrl}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Compliments here:</h1>
+        <Compliment currentCompliment={this.state.currentCompliment} />
+        <UrlForm />
+        {/* <Picture /> */}
+      </div>
+    );
   }
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,45 @@ import React, { Component } from "react";
 import { Form } from "semantic-ui-react";
 
 class UrlForm extends Component {
+  state = {
+    imgUrl: ""
+  };
+
+  handleChange = event => {
+    // debugger
+    this.setState({
+      imgUrl: event.target.value
+    });
+  };
+
+  handleSubmit = e => {
+      debugger
+    e.preventDefault();
+    fetch("http://localhost:3002/api/v1/images", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        url: this.state.imgUrl,
+        user_id: 1
+      })
+    }).then(resp => resp.json());
+  };
+
   render() {
     return (
       <div>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group>
-            <Form.Input placeholder="Image Url" name="imgUrl" value={this.props.imgUrl} />
+            <Form.Input
+              type="text"
+              placeholder="Image Url"
+              name="imgUrl"
+              value={this.state.imgUrl}
+              onChange={this.handleChange}
+            />
             <Form.Button content="Submit" />
           </Form.Group>
         </Form>
