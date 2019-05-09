5f6980eeb1ed2f0b344bbcbd147a4086245c493a
schazbot <charlotte.pearce1984@gmail.com>
2019-05-09 15:53:22 +0100

    MVP achieved

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Button from "./Button";
+import { Image} from 'semantic-ui-react'
+
 
 class Picture extends Component {
   render() {
     return (
       <>
         <div className="picture-container">
-          <img className="picdiv" src={this.props.currentImage} width="300" alt="" />
+          <Image size='large'className="picdiv" src={this.props.currentImage} alt="" />
           <Button className="button-container" resetCurrentImageState={this.props.resetCurrentImageState} />
         </div>
       </>
